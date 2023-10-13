53c82ee4cbdb242b670874166fd2fcddbfbebf81
Ludmila <ludmila.drapac@cnas.gov.md>
2023-10-13 03:33:11 -0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import person.Person;
+import person.Staff;
+import person.Student;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("Victor", "Alba Iulia");
+        Staff staff = new Staff("Dana", "Teodorovici", "DanteAlighere", 21.50);
+        Student student = new Student("Daniel", "Docuceaev", "Informatica", 2023, 1000.50);
+
+        System.out.println(person.getName());
+        System.out.println(person.getAddress());
+        System.out.println("_________________________________");
+
+        System.out.println(staff.getName());
+        System.out.println(staff.getAddress());
+        System.out.println(staff.getSchool());
+        System.out.println(staff.getPay());
+        System.out.println("_________________________________");
+
+        System.out.println(student.getName());
+        System.out.println(student.getAddress());
+        System.out.println(student.getProgram());
+        System.out.println(student.getYear());
+        System.out.println(student.getFee());
+
     }
 }
+
